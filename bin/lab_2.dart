40170adbd8d2d53188c4081bc20003cc9b983867
noorahmed1000 <70135665@student.uol.edu.pk>
2024-09-25 10:51:12 +0500

    All labs in one file

---
+++ bin/lab_2.dart
@@ -0,0 +1,96 @@
+ /*
+ int main(){
+   int a = 10;
+   int b = 5;
+   var div = a~/b;
+   print(div);
+   return 0;
+  }*/
+
+  // int main(){
+  //   int a=10;
+  //   print(-a);
+  //   return 0;
+  // }
+
+  // int main(){
+  //   int a = 30;
+  //   int b = 50;
+  //   print(a<=b?true:false) ;
+  
+  //   return 0;
+  // }
+
+  // int main(){
+  //   int a=10;
+  //   int b=5;
+  //   print(a==b?'equal':'not equal');
+  //   return 0;
+  // }
+
+
+  //   int main(){
+  //   int a=10;
+  //   int b=50;
+  //   print(a==b?'NOT equal':'equal');
+  //   return 0;
+  // }
+
+  // int main(){
+  //   var a;
+  //   a=50;
+  //   print(a);
+  //   return 0;
+  // }
+
+  // int main(){
+  //   int age=18;
+  //   String eligble = 'citizen';
+  //   if(age>=18){
+  //     print('The person is 18 years old $age');
+  //     print('The person is also a citzen $eligble');
+  //   }
+  //   else{
+  //     print('invalid citizen');
+  //   }
+  //   return 0;
+  // }
+
+  // int main(){
+  //   String member = 'Premium Member';
+  //   int amount = 100;
+  //   if(member=='Premium Member' && amount==100){
+  //     print('The member of the group is $member');
+  //     print('The member is eligible for a discount of $amount');
+  //   }
+  //   else{
+  //     print('Not a premium member');
+  //   }
+  //   return 0;
+  // }
+  // int main()
+  // {
+  //  
+  // String member = 'Premium Member';
+  //   int amount = 100;
+  //   print((member=='Premium Member' && amount==100)? 'The member is a $member and it is eligble for a discount of $amount':'Sorry man you are not a $member');
+  
+  // return 0;}
+  // int main()
+  // {
+  //   String user = 'Subscribed';
+  //   print((user=='Subscribed')?'The user is $user and is eligible for a news letter':'It is not eligible');
+  //   return 0;
+  // }
+
+  // void main(){
+  //   var age =18;
+  //   print((age==18)?"eligible to vote":"not eligible");
+
+  // }
+
+  // void main(){
+  //   var discount = 100;
+  //   print((discount==100)?'eligible to vote':'not eligible');
+
+  // }
