6fd88706df06a648262e986c03a6281f02299796
noorahmed1000 <70135665@student.uol.edu.pk>
2024-11-02 14:33:40 +0500

    updated circle task

--- Flutter_lab_13_assignment_task/lib/circle.dart
+++ Flutter_lab_13_assignment_task/lib/circle.dart
@@ -10,58 +10,58 @@ void main(){
             ),
             body:Center(
               child: Container(
-                width: 500,
+                width: 600,
                 height: 500,
                 padding: EdgeInsets.all(20),
                 margin: EdgeInsets.all(10),
                 decoration: BoxDecoration(
                   color: Colors.black,
-                  shape: BoxShape.circle,
+                  borderRadius: BorderRadius.circular(300),
                 ),
                 child: Container(
-                  width: 400,
+                  width: 500,
                   height: 400,
                   padding: EdgeInsets.all(20),
                   margin: EdgeInsets.all(10),
                   decoration: BoxDecoration(
                     color: Colors.blueAccent,
-                    shape: BoxShape.circle,
+                    borderRadius: BorderRadius.circular(200),
                   ),
                   child: Container(
-                    width: 300,
+                    width: 400,
                     height: 300,
                     padding: EdgeInsets.all(20),
                     margin: EdgeInsets.all(10),
                     decoration: BoxDecoration(
                       color: const Color.fromARGB(255, 214, 142, 47),
-                      shape: BoxShape.circle,
+                      borderRadius: BorderRadius.circular(200),
                     ),
                     child: Container(
-                      width: 200,
+                      width: 300,
                       height: 200,
                       padding: EdgeInsets.all(20),
                       margin: EdgeInsets.all(10),
                       decoration: BoxDecoration(
                         color: Colors.green,
-                        shape: BoxShape.circle,
+                        borderRadius: BorderRadius.circular(200),
                       ),
                       child: Container(
-                        width: 100,
+                        width: 200,
                         height: 100,
                         padding: EdgeInsets.all(20),
                         margin: EdgeInsets.all(10),
                         decoration: BoxDecoration(
                           color: Colors.grey,
-                          shape: BoxShape.circle,
+                          borderRadius: BorderRadius.circular(200),
                         ),
                         child: Container(
-                          width: 50,
+                          width: 100,
                           height: 50,
                           padding: EdgeInsets.all(20),
                           margin: EdgeInsets.all(10),
                           decoration: BoxDecoration(
                             color: Colors.white,
-                            shape: BoxShape.circle,
+                            borderRadius: BorderRadius.circular(200),
                           ),
                         ),
                       ),
